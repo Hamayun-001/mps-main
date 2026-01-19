05d6e06ae240ee186385c26889887dc80c5dbbfb
razakhansa <mrazakhan8921@gmail.com>
2026-01-19 14:55:09 +0500

    Hero Section done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Faq from "@/components/FAQ";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center  font-sans">
       <main className="flex min-h-screen w-full  flex-col items-center justify-between bg-white  sm:items-start">
+        <HeroSection />
         <Faq />
       </main>
     </div>
