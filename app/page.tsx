d3ae60c9573c9beaa02581a689db14d1856f89f8
razakhansa <mrazakhan8921@gmail.com>
2026-01-19 13:57:54 +0500

    catalog done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import CatalogBar from "@/components/CatalogBar";
 import Faq from "@/components/FAQ";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center  font-sans">
       <main className="flex min-h-screen w-full  flex-col items-center justify-between bg-white  sm:items-start">
+        <CatalogBar />
         <Faq />
       </main>
     </div>
